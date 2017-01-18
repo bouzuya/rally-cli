5d220ef031546a898ac9861d0cefb388426fc57d
bouzuya <m@bouzuya.net>
2017-01-19 08:52:46 +0900

    show args

--- src/Main.purs
+++ src/Main.purs
@@ -3,9 +3,11 @@ module Main where
 import Prelude (Unit, ($), (<>), bind, show)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
+import Data.Array (drop) as Array
 import Node.Process (PROCESS, argv) as Process
 
 main :: forall e. Eff (console :: CONSOLE, process :: Process.PROCESS | e) Unit
 main = do
   argv <- Process.argv
-  log $ "argv: " <> show argv
+  let args = Array.drop 2 $ argv
+  log $ "args: " <> show args
