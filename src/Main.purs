834f266f1aef30a1435bdfa76c1a8a83b41417a8
bouzuya <m@bouzuya.net>
2017-01-19 09:05:15 +0900

    add data Command

--- src/Main.purs
+++ src/Main.purs
@@ -1,16 +1,25 @@
 module Main where
 
-import Prelude (Unit, ($), (<>), bind, id, show)
+import Prelude (class Show, Unit, ($), (<>), bind, id, show)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
-import Data.Array (drop, head) as Array
+import Data.Array (drop, head, tail) as Array
 import Data.Maybe (maybe)
 import Node.Process (PROCESS, argv) as Process
+
+data Command = Command String (Array String)
+
+instance commandShow :: Show Command where
+  show (Command name args) = name <> " " <> show args
+
+command :: Array String -> Command
+command argv = Command name commandArgs
+  where
+    args = Array.drop 2 argv
+    name = maybe "help" id $ Array.head args
+    commandArgs = maybe [] id $ Array.tail args
 
 main :: forall e. Eff (console :: CONSOLE, process :: Process.PROCESS | e) Unit
 main = do
   argv <- Process.argv
-  let args = Array.drop 2 $ argv
-  let subcommand = maybe "help" id $ Array.head args
-  log $ "args: " <> show args
-  log $ "subcommand: " <> subcommand
+  log $ show $ command argv
