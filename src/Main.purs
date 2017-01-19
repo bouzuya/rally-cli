26708268de91dda4a5f5fd3162758bc684bac367
bouzuya <m@bouzuya.net>
2017-01-19 13:54:09 +0900

    add help command

--- src/Main.purs
+++ src/Main.purs
@@ -4,9 +4,19 @@ import Prelude (Unit, ($), bind, show)
 import Command (command)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
-import Node.Process (PROCESS, argv) as Process
+import Node.Process (PROCESS, argv, exit) as Process
 
-main :: forall e. Eff (console :: CONSOLE, process :: Process.PROCESS | e) Unit
+type Effs = (console :: CONSOLE, process :: Process.PROCESS)
+
+help :: Array String -> Eff Effs Unit
+help _ = do
+  log "rally <subcommand> [<args>]"
+  log ""
+  log "  help ... Show help"
+  Process.exit 0
+
+main :: Eff Effs Unit
 main = do
   argv <- Process.argv
   log $ show $ command argv
+  help []
