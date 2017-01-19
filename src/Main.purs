c7e1e375685acd69841a6ad0beed580bf7ac8792
bouzuya <m@bouzuya.net>
2017-01-19 14:00:37 +0900

    add export command (dummy)

--- src/Main.purs
+++ src/Main.purs
@@ -1,22 +1,29 @@
 module Main where
 
-import Prelude (Unit, ($), bind, show)
-import Command (command)
+import Prelude (Unit, ($), (==), bind)
+import Command (args, command, name)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Node.Process (PROCESS, argv, exit) as Process
 
 type Effs = (console :: CONSOLE, process :: Process.PROCESS)
 
+export :: Array String -> Eff Effs Unit
+export _ = do
+  log "(export)"
+  Process.exit 0
+
 help :: Array String -> Eff Effs Unit
 help _ = do
-  log "rally <subcommand> [<args>]"
+  log "Usage: rally <command> [<args>]"
   log ""
-  log "  help ... Show help"
+  log "Commands:"
+  log "  export"
+  log "  help"
   Process.exit 0
 
 main :: Eff Effs Unit
 main = do
   argv <- Process.argv
-  log $ show $ command argv
-  help []
+  let c = command argv
+  if name c == "export" then export $ args c else help []
