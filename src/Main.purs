08a83ebd894b9275ffe5806bb3d9d05f95c60fcd
bouzuya <m@bouzuya.net>
2017-01-20 14:12:27 +0900

    add dummy fetch to export command

--- src/Main.purs
+++ src/Main.purs
@@ -1,17 +1,26 @@
 module Main where
 
-import Prelude (Unit, ($), (==), bind)
+import Prelude (Unit, ($), (==), (<>), bind, void)
 import Command (args, command, name)
+import Control.Monad.Aff (launchAff)
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Exception (EXCEPTION)
+import Data.Options ((:=))
+import Fetch (HTTP, fetch)
+import Fetch.Options (Method(..), method, url) as FetchOptions
 import Node.Process (PROCESS, argv, exit) as Process
 
-type Effs = (console :: CONSOLE, process :: Process.PROCESS)
+type Effs = (console :: CONSOLE, err :: EXCEPTION, http :: HTTP, process :: Process.PROCESS)
 
 export :: Array String -> Eff Effs Unit
-export _ = do
-  log "(export)"
-  Process.exit 0
+export _ = void $ launchAff do
+  let url = "https://api.rallyapp.jp/rallies/kqahbgshyjwrbzwh"
+  text <- fetch $ FetchOptions.method := FetchOptions.GET
+                  <> FetchOptions.url := url
+  liftEff $ log text
+  liftEff $ Process.exit 0
 
 help :: Array String -> Eff Effs Unit
 help _ = do
