b706827d5892cda648ff94d13491d0eb36495894
bouzuya <m@bouzuya.net>
2017-01-29 14:28:13 +0900

    refactor

--- src/Main.purs
+++ src/Main.purs
@@ -1,42 +1,69 @@
 module Main where
 
 import Command (args, command, name)
-import Control.Monad.Aff (launchAff)
+import Control.Monad.Aff (Aff, launchAff)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Eff.Exception (EXCEPTION)
 import Data.CreateTokenResponse (CreateTokenResponse(..))
+import Data.GetStampRallyResponse (GetStampRallyResponse)
 import Data.Maybe (maybe)
 import Data.StrMap (lookup) as StrMap
 import Fetch (HTTP)
 import Node.Process (PROCESS, argv, getEnv, exit) as Process
-import Prelude (Unit, ($), (==), bind, id, show, void)
+import Prelude (Unit, ($), (==), bind, id, pure, show, void)
 import Request.CreateToken (createToken)
 import Request.GetStampRally (getStampRally)
 
-type Effs = (console :: CONSOLE, err :: EXCEPTION, http :: HTTP, process :: Process.PROCESS)
+getStampRally' :: forall eff
+                  . CreateTokenResponse
+                  -> String
+                  -> Aff ( http :: HTTP
+                         | eff
+                         ) GetStampRallyResponse
+getStampRally' (CreateTokenResponse { token }) stampRallyId = do
+  getStampRally stampRallyId token
 
 -- process.env.EMAIL='<email>'
 -- process.env.PASSWORD='<password>'
 -- process.env.STAMP_RALLY_ID='<stamp_rally_id>'
-export :: Array String -> Eff Effs Unit
-export _ = void $ launchAff do
-  env <- liftEff $ Process.getEnv
+params :: forall eff
+          . Eff ( process :: Process.PROCESS
+                | eff
+                ) { email :: String
+                  , password :: String
+                  , stampRallyId :: String
+                  }
+params = do
+  env <- Process.getEnv
   let email = maybe "" id $ StrMap.lookup "EMAIL" env
   let password = maybe "" id $ StrMap.lookup "PASSWORD" env
+  let stampRallyId = maybe "" id $ StrMap.lookup "STAMP_RALLY_ID" env
+  pure { email, password, stampRallyId }
+
+export :: forall eff
+          . Array String
+          -> Eff ( console :: CONSOLE
+                 , err :: EXCEPTION
+                 , http :: HTTP
+                 , process :: Process.PROCESS
+                 | eff
+                 ) Unit
+export _ = void $ launchAff do
+  { email, password, stampRallyId } <- liftEff $ params
   token <- createToken email password
   liftEff $ log $ show token
-  do
-    let stampRallyId = maybe "" id $ StrMap.lookup "STAMP_RALLY_ID" env
-    liftEff $ log stampRallyId
-    stampRally <- getStampRally stampRallyId $ accessToken token
-    liftEff $ log $ show stampRally
+  stampRally <- getStampRally' token stampRallyId
+  liftEff $ log $ show stampRally
   liftEff $ Process.exit 0
-  where
-    accessToken (CreateTokenResponse { token }) = token
 
-help :: Array String -> Eff Effs Unit
+help :: forall eff
+        . Array String
+        -> Eff ( console :: CONSOLE
+               , process :: Process.PROCESS
+               | eff
+               ) Unit
 help _ = do
   log "Usage: rally <command> [<args>]"
   log ""
@@ -45,7 +72,13 @@ help _ = do
   log "  help"
   Process.exit 0
 
-main :: Eff Effs Unit
+main :: forall eff
+        . Eff ( console :: CONSOLE
+              , err :: EXCEPTION
+              , http :: HTTP
+              , process :: Process.PROCESS
+              | eff
+              ) Unit
 main = do
   argv <- Process.argv
   let c = command argv
