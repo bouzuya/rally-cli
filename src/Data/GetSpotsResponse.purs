7705cb8684007bdf9f53ee65c904f00dba867be1
bouzuya <m@bouzuya.net>
2017-01-31 13:53:03 +0900

    add Data.GetSpotsResponse

---
+++ src/Data/GetSpotsResponse.purs
@@ -0,0 +1,27 @@
+module Data.GetSpotsResponse where
+
+import Data.Foreign.Class (class IsForeign, read, readProp)
+import Data.Foreign.Index (prop)
+import Data.Show (class Show, show)
+import Prelude (($), (<>), bind, map, pure)
+
+data GetSpotsItem = GetSpotsItem { id :: Int }
+data GetSpotsResponse = GetSpotsResponse { ids :: Array GetSpotsItem }
+
+instance getSpotsItemShow :: Show GetSpotsItem where
+  show (GetSpotsItem { id }) = "(GetSpotsItem { id: " <> show id <> " })"
+
+instance getSpotsItemIsForeign :: IsForeign GetSpotsItem where
+  read value = do
+    id <- readProp "id" value
+    pure $ GetSpotsItem { id }
+
+instance getSpotsResponseShow :: Show GetSpotsResponse where
+  show (GetSpotsResponse { ids }) =
+    "(GetSpotsResponse " <> show (map show ids) <> ")"
+
+instance getSpotsResponseIsForeign :: IsForeign GetSpotsResponse where
+  read value = do
+    spotsForeign <- prop "spots" value
+    ids <- read spotsForeign
+    pure $ GetSpotsResponse { ids }
